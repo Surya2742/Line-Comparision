34c4572bccf85bc13339296df049721ee8aea080
Surya2742 <boby7724284@gmail.com>
2022-07-15 21:49:19 +0530

    UC1-Length_Calculation

--- UC1.java
+++ UC1.java
@@ -1,9 +1,9 @@
 public class UC1 {
 		
 public static void main(String[] args) {
-	UC1 Line_Comparision = new UC1();
-	double computeDistance = Line_Comparision.Co_Ordinates(1, 5, 3, 4);
-	System.out.println("Distance between two point is : " + computeDistance);
+	UC1 Calculation = new UC1();
+	double Length = Calculation.Co_Ordinates(1, 5, 3, 4);
+	System.out.println("Distance between two point is : " + Length);
 }
 
 double Co_Ordinates (int x1, int x2, int y1, int y2)
